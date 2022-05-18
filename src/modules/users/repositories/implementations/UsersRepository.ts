7344e4af32f369bcd9a2ef222aaf5cf96e645415
Guedes <gguilhermegguedes@gmail.com>
2022-05-18 15:57:18 -0300

    concluido

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,3 +1,4 @@
+import { clearScreenDown } from 'readline';
 import { getRepository, Repository } from 'typeorm';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
@@ -14,17 +15,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository.findOneOrFail({id: user_id}, {relations: ["games"]});
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * 
+      FROM users 
+      ORDER BY first_name ASC`
+    ); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * 
+      FROM users 
+      WHERE LOWER(first_name)=LOWER('${first_name}') AND LOWER(last_name)=LOWER('${last_name}') LIMIT 1`
+    ); 
   }
 }
